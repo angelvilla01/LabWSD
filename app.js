d79171ba307fc1680df9e2f5f44a94c231b6ab36
angelvilla01 <angel.villafranca@alu.uclm.es>
2024-03-15 20:29:48 +0100

    Add public\styles.csss

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('uploads'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Rutas
 import { notesRouter } from './routes/notesRoutes.js';
